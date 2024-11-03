e36f037ff689098faf026d222ece9259d702c24c
Micheal <khanzaidan786@gmail.com>
2024-11-03 14:12:39 +0530

    schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { startup } from "./startups";
+import { author } from "./author";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startup],
+};
